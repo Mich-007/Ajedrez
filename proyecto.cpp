0831436c4c0e5bf1c0ac9e132bd73cde7907ec6e
nombre_usuario <email_usuario>
2021-09-04 22:20:28 -0600

    archivo

---
+++ proyecto.cpp
@@ -0,0 +1,7 @@
+//Proyecto 1
+//Curso: Análisis y diseños de Algoritmos.
+//Profesor: Cristopher Chanto.
+//Estudiantes: Michelle Gutiérrez Araya y María Paulina Camacho Herrera.
+//Actividad: realizar un ajedrez en modo texto.
+
+//BIBLIOTECAS A USAR EN EL CÓDIGO:
