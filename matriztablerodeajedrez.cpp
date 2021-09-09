f3044e23fba9d68676c21d55a029f2e5cd469164
Fabio Villalobos <fabio0610@hotmail.com>
2021-09-09 15:49:49 -0600

    pequeños cambios

--- matriztablerodeajedrez.cpp
+++ matriztablerodeajedrez.cpp
@@ -4,37 +4,45 @@
 #include "conio.h"
 #include <fstream>
 using namespace std;
+string tablero[8][8] ={" "};
+string eleccion[4]={"Ar","Ab","D","I"};
+string piezas[6]={""};
+
+/*void mov_Rey(){
+
+}*/
+
 
 int main(){
-    string tablero[8][8] ={" "};
-    ofstream file;
-    file.open("tablero.txt");
+    cout << "Elija la pieza que va a mover: "<<endl;
+    //cin >>;
+
     for(int i = 0; i <8; i++){
         for(int j=0; j<8; j++){
-        	
+
 
             if(j == 1 || j == 6){
-                tablero[i][j] = 'P';
+                tablero[i][j] = "P";
 
 
             }else if((i==0 || i ==7) && (j==0|| j==7)){
-                tablero[i][j]='T';
+                tablero[i][j]="T";
 
 
             }else if((i==1 || i ==6) && (j==0|| j==7)){
-                tablero[i][j] = 'C';
+                tablero[i][j] = "C";
 
 
             }else if((i==2 || i ==5) && (j==0|| j==7)){
-                tablero[i][j] = 'A';
+                tablero[i][j] = "A";
 
 
             }else if(i==3  && (j==0|| j==7)){
-                tablero[i][j]= 'D';
+                tablero[i][j]= "D";
 
 
             }else if( i == 4  && (j==0 || j==7 )){
-                tablero[i][j]= 'R';
+                tablero[i][j]= "R";
 
 
             }
@@ -42,10 +50,12 @@ int main(){
         }
     }
 
+    ofstream file;
+    file.open("tablero.txt");
     for(int i = 0; i<8; i++){
         for(int j =0; j<8; j++){
             file << tablero[i][j]<<"__|";
-            
+
         }
         file <<endl;
     }
