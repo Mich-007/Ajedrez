895d51518d8614d4b4e1b385bcc07c0300a9600e
nombre_usuario <email_usuario>
2021-09-13 01:27:47 -0600

    acerca_de

---
+++ Acercade.cpp
@@ -0,0 +1,24 @@
+//Bibliotecas a usar
+#include <iostream>
+#include <string>
+#include <stdio.h>
+
+using namespace std;
+
+//Funcion principal
+int main(){
+    cout << "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*" << endl;
+    cout << "Instituto Tecnologico de Costa Rica." << endl;
+    cout << "Sede Interuniversitaria de Alajuela." << endl;
+    cout << "Analisis y disenno de algoritmos." << endl;
+    cout << "Proyecto #1" << endl;
+    cout << "Estudiantes: Michelle Gutierrez Araya." << endl;
+    cout << "             Carnet: 2021437374 "<< endl;
+    cout << "             Maria Paulina Camacho Herrera." << endl;
+    cout << "             Carnet: 2021093601" << endl;
+    cout << "Grupo #1." << endl;
+    cout << "II Semestre." << endl;
+    cout << "Anno: 2021" << endl;
+    cout << "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*" << endl;
+    return 0;
+}
